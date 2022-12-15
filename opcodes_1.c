355a3075127ee538dfe5da7ebd6561e5945802df
vickouma77 <vickprestone20@gmail.com>
2022-12-15 12:15:33 -0800

    adding pint,swap and nop command

--- opcodes_1.c
+++ opcodes_1.c
@@ -67,3 +67,69 @@ void pall(stack_t **stack, unsigned int line_number)
 	}
 	printf("%d\n", temp->n);
 }
+
+
+/**
+ * pint - Print the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+
+	stack_t *temp = NULL;
+
+	if (stack == NULL || *stack == NULL)
+	{
+		pint_e(line_number);
+		return;
+	}
+	temp = *stack;
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	printf("%d", temp->n);
+	printf("\n");
+}
+
+/**
+ * swap - Print the stack
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void swap(stack_t **stack, unsigned int line_number)
+{
+	stack_t *temp;
+	int i, j;
+
+	if (*stack == NULL || stack == NULL)
+		op_e(line_number, "swap");
+
+	temp = (*stack)->next;
+	if ((*stack)->next == NULL)
+		op_e(line_number, "swap");
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+	i = temp->n;
+	j = temp->prev->n;
+	temp->n = j;
+	temp->prev->n = i;
+}
+/**
+ * nop - does not do anything
+ * @stack: head of linkedlist
+ * @line_number: line number of the instruction
+ *
+ * Return: No return
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
